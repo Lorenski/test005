c598eccfb4af4d09a32a351b13b1021770ff85c6
Lorenski2 <pi.sypek@gmail.com>
2016-08-12 15:33:19 +0200

    added run2.cpp

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # test005
 dummy test
 
+qq
---
+++ src/run2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "common.h"
+
+int main(){
+    int a = 17,
+        b = 16;
+    printf( "\n[run2] add(%d,%d) = %d\n\n", a, b, add(a,b) );
+
+    return 0;
+}
+
